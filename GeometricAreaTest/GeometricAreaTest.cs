2ecc9973e6dd75ac3e084ca3bf238bf547cac670
Shuhratzhan Nizamov <shukhratzhon.nizamov@nat.kz>
2023-04-26 16:41:00 +0600

    Issue Бибилиотека  которая умеет вычислять площадь
    C# библиотека для поставки внешним клиентам, которая умеет вычислять площадь круга по радиусу и треугольника по трем сторонам.
    
    1. Добавил "GeometricAreaTest.csproj"

---
+++ GeometricAreaTest/GeometricAreaTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeometricArea.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometricArea.Shapes.Tests
+{
+    [TestClass()]
+    public class GeometricAreaTest
+    {
+        [TestMethod()]
+        public void CircleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAreaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetPerimeterTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
